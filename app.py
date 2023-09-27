fa458425e52c062125de35efc968f78ceae05423
Azfar-Sphere <s.azfar.rahman@gmail.com>
2023-09-27 13:04:03 +0400

    Added Adding Tasks and Deleting Them

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, send_from_directory
+from flask import Flask, render_template, url_for, send_from_directory, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -15,9 +15,31 @@ class Todo(db.Model):
         return "<task %r>" % self.id
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        task_content = request.form.get("content")
+        new_task = Todo(content = task_content)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect(url_for("index"))
+        except:
+            return "Error Adding Task"
+        
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html", tasks=tasks)
+    
+@app.route("/delete/<int:id>")
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect(url_for("index"))
+    
+    except:
+        return "Error Deleting Task"
